ead796f36aba2d9c57e9396d45ae4ef23380d28b
Michael Iglesias <mci12@my.fsu.edu>
2013-12-17 20:07:43 -0500

    api changes

--- api/index.php
+++ api/index.php
@@ -29,71 +29,172 @@ $app = new \Slim\Slim();
 
 // GET: /packages -> Get Packages for particular tenant
 $app->get('/packages/:id', function ($id) {
-	$sql = "SELECT * FROM PackageDelivered WHERE pack_pickedup='no' AND tnt_id=$id";
-	try {
-		$db = getConnection();
-		$q = $db->query($sql);
-                if($q->num_rows > 0) {
-                    while($row = $q->fetch_array(MYSQLI_ASSOC)) {
-                        $rows[] = $row;
-                    }
-                    // Format Response
-                    $data['status'] = 'success';
-                    $data['pending_package_pickups'] = $q->num_rows;
-                    $data['data'] = $rows;
+    $sql = "SELECT * FROM PackageDelivered WHERE pack_pickedup='no' AND tnt_id=$id";
+    try {
+        $db = getConnection();
+        $q = $db->query($sql);
+        if($q->num_rows > 0) {
+            while($row = $q->fetch_array(MYSQLI_ASSOC)) {
+                $rows[] = $row;
+            }
+            // Format Response
+            $data['status'] = 'success';
+            $data['pending_package_pickups'] = $q->num_rows;
+            $data['data'] = $rows;
 
-                } else {
-                    // Format Response
-                    $data['status'] = 'success';
-                    $data['pending_package_pickups'] = $q->num_rows;
-                    $data['data'] = NULL;
-                }
-                // Free result and close connection
-		$q->free();
-		$db->close();
-                
-		echo json_encode($data);
-	} catch (PDOException $e) {
-		echo '{"error"}' . $e->getMessage();
-	}
+        } else {
+            // Format Response
+            $data['status'] = 'success';
+            $data['pending_package_pickups'] = $q->num_rows;
+            $data['data'] = NULL;
+        }
+        // Free result and close connection
+        $q->free();
+        $db->close();
+
+        echo json_encode($data);
+    } catch (PDOException $e) {
+        echo '{"error"}' . $e->getMessage();
+    }
 }); // ***END  GET: /packages/:id -> Get all pending packages for particular tenant
 
 // GET: /packages/history -> Get package pickup history limit 5
 $app->get('/packages/history/:id', function ($id) {
-	$sql = "SELECT * FROM PackageDelivered WHERE pack_pickedup='yes' AND tnt_id=$id ORDER BY pack_date_pickedup LIMIT 5";
-	try {
-		$db = getConnection();
-		$q = $db->query($sql);
-                if($q->num_rows > 0) {
-                    while($row = $q->fetch_array(MYSQLI_ASSOC)) {
-                            $rows[] = $row;
-                    }
-                    // Format Response
-                    $data['status'] = 'success';
-                    $data['package_history'] = $q->num_rows;
-                    $data['response'] = $rows;
-                } else {
-                    $data['status'] = 'success';
-                    $data['package_history'] = $q->num_rows;
-                    $data['response'] = NULL;
-                }
-                // Free result and close connection
-		$q->free();
-		$db->close();
-                
-		echo json_encode($data);
-	} catch (PDOException $e) {
-		echo '{"error"}' . $e->getMessage();
-	}
+    $sql = "SELECT * FROM PackageDelivered WHERE pack_pickedup='yes' AND tnt_id=$id ORDER BY pack_date_pickedup LIMIT 5";
+    try {
+        $db = getConnection();
+        $q = $db->query($sql);
+        if($q->num_rows > 0) {
+            while($row = $q->fetch_array(MYSQLI_ASSOC)) {
+                    $rows[] = $row;
+            }
+            // Format Response
+            $data['status'] = 'success';
+            $data['package_history'] = $q->num_rows;
+            $data['data'] = $rows;
+        } else {
+            $data['status'] = 'success';
+            $data['package_history'] = $q->num_rows;
+            $data['data'] = NULL;
+        }
+        // Free result and close connection
+        $q->free();
+        $db->close();
+
+        echo json_encode($data);
+    } catch (PDOException $e) {
+        echo '{"error"}' . $e->getMessage();
+    }
 }); // ***END  GET: /packages/history/:id -> Get package history limit 5
 
 /**
- * Login ROUTES
+ * MAINTENANCE ROUTES
  *
- * 1. POST -> /login
- * 
+ * 1. /maintenance -> POST
+ * 2. /maintenance/:id -> GET
+ * 3. /maintenance/history/:id
  */
 
+$app->post('/maintenance', function() {
+    $request = \Slim\Slim::getInstance()->request();
+    $body = $request->getBody();
+    $ticket = json_decode($body);
+    
+    $db = getConnection();
+    $tnt_id = $db->real_escape_string($ticket->tnt_id);
+    
+    $sql = "SELECT tun_id FROM Leasing WHERE tnt_id=$tnt_id";
+    $q = $db->query($sql);
+    if($q->num_rows > 0) {
+        while($row = $q->fetch_array(MYSQLI_ASSOC)) {
+            $tun_id = $row['tun_id'];
+        }
+        $sql = "INSERT INTO MaintenanceTicket (tnt_id, tun_id, mticket_status, mticket_title, mticket_description, mticket_open_date) VALUES (?, ?, ?, ?, ?, ?)";
+        try {
+            $stmt = $db->prepare($sql);
+            $stmt->bind_param('iissss', $tnt_id, $tun_id, 'open', $db->real_escape_string($ticket->title), $db->real_escape_string($ticket->description), date("Y-m-d H:i:s"));
+            $stmt->execute();
+            $contact->id = $stmt->insert_id;
+            $db->close();
+        } catch (Exception $e) {
+            echo 'Error: ' . $e->getMessage();
+        }
+        // Format Response
+        $data['status'] = 'success';
+    } else {
+        // Format Response
+        $data['status'] = 'failure';
+    }
+    
+    echo json_encode($data);
+});
+
+// GET - /maintenance/:id
+$app->get('/maintenance/:id', function($id) {
+    $sql = "SELECT * FROM MaintenanceTicket WHERE mticket_status='open' AND tnt_id=$id";
+    try {
+        $db = getConnection();
+        $q = $db->query($sql);
+        if($q->num_rows > 0) {
+            while($row = $q->fetch_array(MYSQLI_ASSOC)) {
+                $rows[] = $row;
+            }
+            // Format Response
+            $data['status'] = 'success';
+            $data['maintenance_requests'] = $q->num_rows;
+            $data['data'] = $rows;
+
+        } else {
+            // Format Response
+            $data['status'] = 'success';
+            $data['maintenance_requests'] = $q->num_rows;
+            $data['data'] = NULL;
+        }
+        // Free result and close connection
+        $q->free();
+        $db->close();
+
+        echo json_encode($data);
+    } catch (PDOException $e) {
+        echo '{"error"}' . $e->getMessage();
+    }
+});
+
+// GET - /maintenance/history/:id
+$app->get('/maintenance/history/:id', function($id) {
+    $sql = "SELECT * FROM MaintenanceTicket WHERE mticket_status='closed' AND tnt_id=$id ORDER BY mticket_closed_date DESC LIMIT 7";
+    try {
+        $db = getConnection();
+        $q = $db->query($sql);
+        if($q->num_rows > 0) {
+            while($row = $q->fetch_array(MYSQLI_ASSOC)) {
+                $rows[] = $row;
+            }
+            // Format Response
+            $data['status'] = 'success';
+            $data['maintenance_requests'] = $q->num_rows;
+            $data['data'] = $rows;
+
+        } else {
+            // Format Response
+            $data['status'] = 'success';
+            $data['maintenance_requests'] = $q->num_rows;
+            $data['data'] = NULL;
+        }
+        // Free result and close connection
+        $q->free();
+        $db->close();
+
+        echo json_encode($data);
+    } catch (PDOException $e) {
+        echo '{"error"}' . $e->getMessage();
+    }
+}); // ***END GET /maintenance/history/:id
+
+/**
+ * Login ROUTES
+ * 1. POST -> /login
+ */
 // POST: /login -> Login User
 $app->post('/login', function() {
     $request = \Slim\Slim::getInstance()->request();
@@ -136,23 +237,23 @@ $app->post('/login', function() {
 
 // POST: /contacts -> Create new contact
 $app->post('/contacts', function() {
-	$request = \Slim\Slim::getInstance()->request();
-	$body = $request->getBody();
-	$contact = json_decode($body);
-	
-	$sql = 'INSERT INTO contacts (fname, lname, email) VALUES (?, ?, ?)';
-	try {
-		$db = getConnection();
-		$stmt = $db->prepare($sql);
-		$stmt->bind_param('sss', $contact->fname, $contact->lname, $contact->email);
-		$stmt->execute();
-		$contact->id = $stmt->insert_id;
-		
-		$db->close();
-		echo json_encode($contact);
-	} catch (Exception $e) {
-		echo 'Error: ' . $e->getMessage();
-	}
+    $request = \Slim\Slim::getInstance()->request();
+    $body = $request->getBody();
+    $contact = json_decode($body);
+
+    $sql = 'INSERT INTO contacts (fname, lname, email) VALUES (?, ?, ?)';
+    try {
+            $db = getConnection();
+            $stmt = $db->prepare($sql);
+            $stmt->bind_param('sss', $contact->fname, $contact->lname, $contact->email);
+            $stmt->execute();
+            $contact->id = $stmt->insert_id;
+
+            $db->close();
+            echo json_encode($contact);
+    } catch (Exception $e) {
+            echo 'Error: ' . $e->getMessage();
+    }
 }); // ***END POST /contacts -> Create new Contact
 
 // DELETE: /contacts/:id -> DELETE a Concact
